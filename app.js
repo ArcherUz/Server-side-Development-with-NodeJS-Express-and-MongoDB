2c7378fef247eacc507c79ce9c535eb70f1353e4
archer <py@ualberta.ca>
2023-01-11 03:11:23 -0700

    favoriteSchema, favoriteRouter, and updated app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 var config = require('./config');
 
 var uploadRouter = require('./routes/uploadRouter');
@@ -93,6 +94,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
